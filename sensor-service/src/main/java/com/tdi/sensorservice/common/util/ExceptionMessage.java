60dcba69ecec598e91c41cb1ea0b73339bafc6c3
Daniil Tereshko <daniiltereshko123@gmail.com>
2025-03-24 17:10:27 +0300

    Утилитные классы и файлы с конфигурацией сообщений при исключениях и ошибках валидации

---
+++ sensor-service/src/main/java/com/tdi/sensorservice/common/util/ExceptionMessage.java
@@ -0,0 +1,8 @@
+package com.tdi.sensorservice.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ExceptionMessage {
+}
---
+++ sensor-service/src/main/java/com/tdi/sensorservice/common/util/ValidationMessage.java
@@ -0,0 +1,22 @@
+package com.tdi.sensorservice.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ValidationMessage {
+
+    public static final String ID_MUST_BE_NULL_ON_CREATE = "validation.sensor.id.must_be_null_on_create";
+    public static final String ID_REQUIRED_ON_UPDATE = "validation.sensor.id.required_on_update";
+
+    /* SensorDto */
+    public static final String NAME_INCORRECT_LENGTH = "validation.sensor.name.incorrect_length";
+    public static final String NAME_FIELD_IS_EMPTY = "validation.sensor.name.empty";
+    public static final String MODEL_INCORRECT_LENGTH = "validation.sensor.model.incorrect_length";
+    public static final String MODEL_FIELD_IS_EMPTY = "validation.sensor.model.empty";
+    public static final String RANGE_FIELD_IS_EMPTY = "validation.sensor.range.empty";
+    public static final String TYPE_FIELD_IS_EMPTY = "validation.sensor.type.empty";
+    public static final String LOCATION_INCORRECT_LENGTH = "validation.sensor.location.incorrect_length";
+    public static final String DESCRIPTION_INCORRECT_LENGTH = "validation.sensor.description.incorrect_length";
+
+}
